ecdc40290b610b2294e7f89f2c0b5f1a8cdc8ced
jvanbaastca <j.vanbaast@citrusandriessen.nl>
2019-02-22 15:24:29 +0100

    Swagger implementatie

--- src/main/java/com/example/demo/controller/GreetingsController.java
+++ src/main/java/com/example/demo/controller/GreetingsController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.repository.GreetingRepository;
 import com.example.demo.model.Greeting;
 import com.example.demo.services.GreetingsService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +17,32 @@ public class GreetingsController {
     private GreetingsService greetingsService;
 
     @GetMapping("")
+    @ApiOperation("Returns all greetings")
     public List<Greeting> all() {
         return greetingsService.findAll();
     }
 
     @PostMapping("/")
+    @ApiOperation("Create a greeting")
     public Greeting newGreeting(@RequestBody Greeting greeting) {
         return greetingsService.save(greeting);
     }
 
     @GetMapping("/{name}")
+    @ApiOperation("Returns a greeting by a specified name")
     public Greeting greetingByName(@PathVariable(value = "name") String name) {
         return greetingsService.findByName(name);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Updates a greeting by a specified name")
     public Greeting updateGreeting(@RequestBody Greeting greeting, @PathVariable("id") String id) {
       greeting.setId(id);
       return greetingsService.save(greeting);
     }
 
     @DeleteMapping("/")
+    @ApiOperation("Deletes a greeting by a specified name")
     public void deleteGreeting(@RequestParam String id) {
         greetingsService.deleteById(id);
     }
--- src/main/java/com/example/demo/repository/GreetingRepository.java
+++ src/main/java/com/example/demo/repository/GreetingRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.model.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface GreetingRepository extends BaseRepository<Greeting, String> {
+public interface GreetingRepository extends MongoRepository<Greeting, String> {
 
     Greeting findByName(String content);
 }
