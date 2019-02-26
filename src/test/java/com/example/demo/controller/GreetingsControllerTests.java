4935620b1f55a7a48b79758d417b19c5f052ff40
jvanbaastca <j.vanbaast@citrusandriessen.nl>
2019-02-26 16:17:11 +0100

    Not working Generates snippets but does not generate the output from the snippets

--- src/test/java/com/example/demo/controller/GreetingsControllerTests.java
+++ src/test/java/com/example/demo/controller/GreetingsControllerTests.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -11,34 +14,47 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.example.demo.model.Greeting;
 import com.example.demo.services.GreetingsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GreetingsController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class GreetingsControllerTests {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @MockBean
     private GreetingsService service;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void greetingByName() throws Exception {
         when(service.findByName("Jeroen")).thenReturn(new Greeting("1", "Jeroen"));
-        this.mockMvc.perform(get("/greetings/Jeroen")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Jeroen")));
+        this.mockMvc.perform(get("/greetings/Jeroen"))
+                .andDo(print())
+                .andExpect(content().string(containsString("Jeroen")))
+                .andDo(document("home", responseFields(
+                        fieldWithPath("name").description("The name of the user to greet."),
+                        fieldWithPath("id").description("The id of the greeting"))));
     }
 
     @Test
